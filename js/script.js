9ad1831e3c95cb6f1b35bd437c9efb1319b72dcc
haokaili <>
2021-05-23 22:17:36 +0000

    finish

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: haokai
+// Created on: May 2021
+// This is the Age-and-Movie
+
+"use strict"
+
+function enterClicked() {
+  //input
+  const age = parseInt(document.getElementById("age").value)
+
+  // process
+  if (age >= 17) {
+    //output that answer of R
+    document.getElementById('answer').innerHTML = "You can see a R movie aline."
+  } else if (age >= 13) {
+    //output that answer of PG-13
+    document.getElementById('answer').innerHTML = "You can see a PG-13 movie aline."
+  } else if (age >= 5) {
+    //output that answer of G or PG
+    document.getElementById('answer').innerHTML = "You can see a G or PG movie aline."
+  } else {
+    //output that answer of too young
+    document.getElementById('answer').innerHTML = "Uh, you are too young for most things."
+  }
+  document.getElementById('thanks').innerHTML = "Thanks for verifying  your age!"
+}
